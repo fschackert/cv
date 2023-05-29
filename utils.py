6f75b3f0927bf35893e4cd8b85e14de0a19625f9
fschackert <florian.schackert@t-online.de>
2023-05-29 21:38:56 +0200

    Change color theme

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 category_to_color = {
-    'MUSIC': 'green',
-    'PHYSICS': 'blue',
-    'SCHOOL': 'red',
-    'WORK': 'orange',
+    'MUSIC': '#1CE822',
+    'PHYSICS': '#2766E8',
+    'SCHOOL': '#E83283',
+    'WORK': '#E8B84A',
 }
