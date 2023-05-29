501fcc68d67596e82459677fe60437d45fc8e89b
fschackert <florian.schackert@t-online.de>
2023-05-29 11:35:22 +0200

    Change behaviour to hover

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import html
+import dash_bootstrap_components as dbc
 from dash import Dash
-#import dash_bootstrap_components as dbc
+from dash import html
 from dash.dependencies import Input, Output, State
 
 from components import app_components
@@ -17,7 +17,10 @@ def main() -> None:
     # GLOBAL THINGIES
     # ==================================================================
 
-    app = Dash(__name__)
+    app = Dash(
+        __name__,
+        external_stylesheets=[dbc.themes.SLATE]
+    )
     app.layout = html.Div(
         children=app_components,
         #style={'display': 'flex', 'flex-direction': 'row'},
@@ -28,17 +31,15 @@ def main() -> None:
         parse_dates=['start', 'end'],
     )
 
-    def _get_selected_items(
-            click_data,
+    def _get_highlighted_locations(
+            hover_data,
             selected_data):
         selected_items = []
+        if hover_data:
+            selected_items += [hover_data['points'][0]['pointNumber']]
         if selected_data:
             selected_items += [point['pointNumber'] for point in selected_data['points']]
-            return selected_items
-        if click_data:
-            selected_items += [click_data['points'][0]['pointNumber']]
-            return selected_items
-        return df.index.to_list()
+        return selected_items
 
     # ==================================================================
     # FIGURE CALLBACKS
@@ -46,12 +47,8 @@ def main() -> None:
 
     @app.callback(
         Output('timeline', 'figure'),
-        Input('timeline', 'clickData'),
-        Input('timeline', 'selectedData'))
-    def update_timeline(
-            click_data,
-            selected_data):
-        selected_items = _get_selected_items(click_data, selected_data)
+        Input('timeline', 'hoverData'))
+    def update_timeline(hover_data):
         timeline = go.Figure(
             layout={
                 'height': 360,
@@ -62,35 +59,39 @@ def main() -> None:
         )
         timeline.add_trace(
             go.Bar(
-                # dash does not seem to like timedelta, maybe related to issue 1808...
-                x=(df['end'] - df['start']).astype('timedelta64[ms]').dt.days*24*60*60*1000,
+                # dash does not seem to like timedelta, maybe related to github.com/plotly/dash/issues/1808
+                x=(df['end'] - df['start']).dt.days*24*60*60*1000,
                 y=df['title'],
                 orientation='h',
                 base=df['start'],
                 marker_color=[category_to_color[category] for category in df['category']],
-                opacity=0.6,
+                opacity=0.8,
             )
         )
-        timeline['data'][0]['marker']['opacity'] = [1.0 if i in selected_items else 0.5 for i in df.index]
+        if hover_data:
+            hover_number = hover_data['points'][0]['pointNumber']
+            timeline['data'][0]['marker']['opacity'] = [1.0 if i == hover_number else 0.5 for i in df.index]
+        else:
+            timeline['data'][0]['marker']['opacity'] = [0.5 for _ in df.index]
         return timeline
 
     @app.callback(
         Output('globe', 'figure'),
-        Input('timeline', 'clickData'),
+        Input('timeline', 'hoverData'),
         Input('timeline', 'selectedData'))
     def update_globe(
-            click_data,
+            hover_data,
             selected_data):
-        scope = df.iloc[_get_selected_items(click_data, selected_data)]
+        scope = df.iloc[_get_highlighted_locations(hover_data, selected_data)]
         globe = px.scatter_geo(
-            data_frame=scope,
+            data_frame=df,
             lat='lat',
             lon='lon',
-            color='category',
-            color_discrete_map=category_to_color,
+            color='title',
+            color_discrete_sequence=[category_to_color[category] for category in df['category']],
             hover_name='location',
-            opacity=0.5,
-            size=(scope['end'] - scope['start']).astype(int),
+            opacity=0.4,
+            size=(df['end'] - df['start']).astype(int),
         )
         globe.update_geos(
             projection_type='orthographic',
@@ -103,6 +104,9 @@ def main() -> None:
             margin={"r": 0, "t": 36, "l": 0, "b": 0},
             showlegend=False
         )
+        for i, point in enumerate(globe['data']):
+            if i in scope.index:
+                point['marker']['opacity'] = 0.8
         return globe
 
     # ==================================================================
--- components.py
+++ components.py
@@ -1,5 +1,6 @@
-from dash import html
 from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
 
 app_components = [
     html.H1(
@@ -9,9 +10,13 @@ app_components = [
     dcc.Graph(
         id='timeline',
         style={'padding': 10, 'flex': 1},
+        clear_on_unhover=True,
     ),
     dcc.Graph(
         id='globe',
         style={'padding': 10, 'flex': 1},
     ),
+    dcc.Store(
+        id='selected_items',
+    )
 ]
