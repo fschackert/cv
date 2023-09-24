8cd7570f7573573c41a677cb9b540ea3782be4de
fschackert <florian.schackert@t-online.de>
2023-09-24 23:10:56 +0200

    Turn off categories

--- hovertemplates.py
+++ hovertemplates.py
@@ -1,7 +1,7 @@
-GLOBE_HOVER = "<b>%{hovertext}</b><br>%{customdata[0]}<b>Start: </b>%{customdata[1]}<br><b>End</b>: %{customdata[2]}<extra>%{customdata[3]}</extra>"
+GLOBE_HOVER = "<b>%{hovertext}</b><br>%{customdata[0]}<b>Start: </b>%{customdata[1]}<br><b>End</b>: %{customdata[2]}<extra></extra>"
 
 TIMELINE_HOVER = {
-    'EDUCATION': "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}<b>Grade</b>: %{customdata[2]}",
-    'MUSIC':     "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}<b>Grade</b>: %{customdata[2]}",
-    'WORK':      "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}",
+    'EDUCATION': "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}<b>Grade</b>: %{customdata[2]}<extra></extra>",
+    'MUSIC':     "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}<b>Grade</b>: %{customdata[2]}<extra></extra>",
+    'WORK':      "<b>%{text}</b><br>%{customdata[0]}%{customdata[1]}<extra></extra>",
 }
